1e43f1023def5298c5e15a7b860a1bf96be9f259
jatin <jatinchoubey29@gmail.com>
2023-09-26 21:16:30 +0530

    Permutate

--- Recursion/02_Permutations.cpp
+++ Recursion/02_Permutations.cpp
@@ -4,28 +4,39 @@ EXAMPLE --
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 */
-class Solution
+#include <bits/stdc++.h>
+using namespace std;
+void helper(vector<int> nums, vector<vector<int>> &ans, int index)
 {
-public:
-    void helper(vector<int> nums, vector<vector<int>> &ans, int index)
+    if (index == nums.size())
     {
-        if (index == nums.size())
-        {
-            ans.push_back(nums);
-            return;
-        }
+        ans.push_back(nums);
+        return;
+    }
 
-        for (int i = index; i < nums.size(); i++)
-        {
-            swap(nums[index], nums[i]);
-            helper(nums, ans, index + 1);
-            swap(nums[index], nums[i]);
-        }
-    }
-    vector<vector<int>> permute(vector<int> &nums)
+    for (int i = index; i < nums.size(); i++)
     {
-        vector<vector<int>> ans;
-        helper(nums, ans, 0);
-        return ans;
+        swap(nums[index], nums[i]);
+        helper(nums, ans, index + 1);
+        swap(nums[index], nums[i]);
     }
-};
+}
+vector<vector<int>> permute(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    helper(nums, ans, 0);
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5, 6};
+    vector<vector<int>> ans = permute(nums);
+    for (auto i : ans)
+    {
+        for (auto j : i)
+            cout << j << " ";
+        cout << endl;
+    }
+    return 0;
+}
