87bccefc0d1bcb3d404ae34578620f0fd18f5bbe
jatin <jatinchoubey29@gmail.com>
2023-10-04 14:11:26 +0530

    Root to Leaf

--- Trees/12_Root_to_Leaf.cpp
+++ Trees/12_Root_to_Leaf.cpp
@@ -1,3 +1,21 @@
+/*
+C++ code to find the path of the given target node from the Root Node.
+
+            3
+          /  \
+        2     5
+      /     /  \
+    1     4     8
+  /           /  \
+0            6    9
+              \    \
+               7    10
+
+Target Node = 6
+
+Output = [3, 5, 8, 6]
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 class node
@@ -52,7 +70,8 @@ void insertNode(node *&root, int data)
     }
 }
 
-void helper(node *root, vector<int> &path, int target, bool &found)
+// ------------- Main Logic Begins here --------------------
+void myLogic(node *root, vector<int> &path, int target, bool &found)
 {
     if (found == true || root == NULL)
         return;
@@ -64,18 +83,39 @@ void helper(node *root, vector<int> &path, int target, bool &found)
         return;
     }
 
-    helper(root->left, path, target, found);
-    helper(root->right, path, target, found);
+    myLogic(root->left, path, target, found);
+    myLogic(root->right, path, target, found);
     if (found == false)
         path.pop_back();
+}
+bool striverLogic(node *root, vector<int> &path, int target)
+{
+    if (root == NULL)
+        return false;
+
+    path.push_back(root->val);
+
+    if (root->val == target)
+        return true;
+
+    bool foundLeft = striverLogic(root->left, path, target);
+    bool foundRight = striverLogic(root->right, path, target);
+
+    if (foundLeft || foundRight)
+        return true;
+
+    path.pop_back();
+    return false;
 }
 vector<int> findPath(node *root, int target)
 {
 
     vector<int> path;
     bool found = false;
-    helper(root, path, target, found);
-    return path;
+    striverLogic(root, path, target);
+    vector<int> path2;
+    myLogic(root, path2, target, found);
+    return path2;
 }
 int main()
 {
@@ -85,12 +125,15 @@ int main()
     insertNode(root, 5);
     insertNode(root, 1);
     insertNode(root, 4);
+    insertNode(root, 8);
     insertNode(root, 6);
-    insertNode(root, 7);
     insertNode(root, 0);
-    insertNode(root, 10);
+    insertNode(root, 7);
     insertNode(root, 9);
-    int target = 9;
+    insertNode(root, 10);
+
+    int target = 6;
+
     vector<int> path = findPath(root, target);
 
     for (auto i : path)
